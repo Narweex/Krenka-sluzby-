36620cd7f8e2fad34155833dfc501f4907a78c0e
Narweex <68287337+Narweex@users.noreply.github.com>
2023-11-09 21:19:31 +0100

    Cteni dat z databaze database.php

--- database.php
+++ database.php
@@ -8,24 +8,45 @@ echo "My first PHP script!";
 ?>
 </h1>
 
+
+<div>
 <?php
 $servername = "localhost";
 $username = "c0dev";
-$password = "bobrjenejlepsi";
+$password = "U_g9h3PzgQwMU";
+$dbName = "c0projectors";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbName);
 
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "Connected successfully" ."<br>";
+
+
+
+$sql = "SELECT * FROM projektory;";
+$result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+$result_check = mysqli_num_rows($result);
+
+if ($result_check > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['nazev'] . "<br>";
+           }
+}
 
 
 
 
 ?>
+</div>
 
 </body>
 </html>
