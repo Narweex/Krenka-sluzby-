f72c986af4a416b9f64c879e6b566319d4fdd5e8
Narweex <68287337+Narweex@users.noreply.github.com>
2023-11-20 17:04:56 +0100

    Opraveno pridavani zarizeni do databaze

--- pridat_zarizeni_do_databaze.php
+++ pridat_zarizeni_do_databaze.php
@@ -8,20 +8,35 @@ include 'config.php';
  $nazev_zarizeni = $_REQUEST['nazev_zarizeni'];
 $mistnost = $_REQUEST['mistnost'];
 $bezdratovy = $_REQUEST['bezdratovy'];
+
 $konektor = $_REQUEST['konektor'];
 
-if ($bezdratovy == 'f'){
-    $bezdratovy = "Ne";
+//if ($konektor == '1'){
+//    $konektor = "1";
+//}
+//else if ($konektor == '2'){
+//    $konektor = "2";
+//}
+//else if ($konektor == '3'){
+//    $konektor = "3";
+//}
+if(DEBUG_MODE){
+    echo $konektor . "<br>";
+}
+
+
+if ($bezdratovy == 'Ne'){
+    $bezdratovy = "f";
 }
 else{
-    $bezdratovy = "Ano";
+    $bezdratovy = "t";
 }
 
 
 $sql = "insert into projektory values ( 'NULL', '$nazev_zarizeni', '$mistnost', '$bezdratovy', '$konektor');";
 
     if(mysqli_query($conn, $sql)){
-        
+
         if(DEBUG_MODE){
         echo "Error: " . mysqli_error($conn);
         }
