842f63ea38606cb013ee95d83b8657a7704ecb67
Narweex <68287337+Narweex@users.noreply.github.com>
2023-11-21 16:18:08 +0100

    Přidán shortcode pro footer
    <?php
    
    include 'footer.php';
    
    footer();
    
    ?>

---
+++ footer.php
@@ -0,0 +1,49 @@
+
+<?php
+
+include 'config.php';
+
+
+    function footer()
+    {
+
+
+        echo " <!-- Footer -->
+
+
+
+                    <footer class='sticky-footer bg-white'>
+
+
+
+                        <div class='container my-auto'>
+
+
+
+                            <div class='copyright text-center my-auto'>
+
+
+
+                                <span>Copyright &copy " . date("Y") . " <a href='https://web-lab.cz/'>Web-Lab.cz</a> & Seminář IVT 2023/24 </span>
+
+
+
+                            </div>
+
+
+
+                        </div>
+
+
+
+                    </footer>
+
+
+
+                    <!-- End of Footer -->";
+
+    }
+
+
+    ?>
+
