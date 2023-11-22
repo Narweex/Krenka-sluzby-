19814c4b2a066532e1308c77213114b86304dd55
tomag999 <tomag2004@gmail.com>
2023-11-22 18:18:53 +0100

    přidání dynamických topbarů

--- oznameni.php
+++ oznameni.php
@@ -4,6 +4,7 @@
 <?php
 
 include 'header.php';
+include 'footer.php';
 
 ?>
 
@@ -183,13 +184,13 @@ include 'header.php';
             <div id="content">
 
 
-                <!-- HEADER -->
+                <!-- Topbar -->
                 <?php
 
                 draw_topbar();
 
                 ?>
-                <!-- END-HEADER -->
+                <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
@@ -219,13 +220,11 @@ include 'header.php';
             </div>
             <!-- End of Main Content -->
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; <a href="https://web-lab.cz/">Web-Lab.cz</a> & Seminář IVT 2023/24 </span>
-                    </div>
-                </div>
-            </footer>
+            <?php
+
+                     footer();
+                
+                ?>
             <!-- End of Footer -->
 
         </div>
