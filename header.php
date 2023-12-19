b7e726647039e67c1ea52a395a3a819cc2901fab
tomag999 <tomag2004@gmail.com>
2023-12-19 11:06:18 +0100

    oprava chyby v odhlašovacím pop-upu

--- header.php
+++ header.php
@@ -122,7 +122,7 @@ function draw_topbar()
                                  aria-labelledby='userDropdown'>
 
 
-                                <a class='dropdown-item' href='#' data-toggle='modal' data-target=''>
+                                <a class='dropdown-item' href='#' data-toggle='modal' data-target='#logoutModal'>
 
                                     <i class='fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400'></i>
                                     Odhlásit se
