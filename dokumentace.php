6d8d32d00fe041971ffd5654a9bcba9bcc177e96
tomag999 <tomag2004@gmail.com>
2023-12-28 15:32:35 +0100

    přidání kontaktní stránky + změny UX v dokumentace.php

--- dokumentace.php
+++ dokumentace.php
@@ -2,10 +2,7 @@
 <html lang="en">
 
 <?php
-include 'config.php';
-include 'connection.php';
-include 'footer.php';
-include 'header.php';
+include 'include.php';
 ?>
 
 <head>
@@ -66,6 +63,12 @@ include 'header.php';
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800">Dokumentace</h1>
 
+                    <!-- Paragraph Již Brzy -->
+                    <p class= 'text-gray-600'>Zde již brzy najdete kompletní uživatelský návod k této webové aplikaci.</p>
+
+                    <!-- Paragraph Kontaktujte nás-->
+                    <p class='text-gray-600'>V případě jakýchkoli dotazů nás neváhejte <a href='kontakt.php'>kontaktovat.</a></p>
+
                 </div>
                 <!-- /.container-fluid -->
 
@@ -95,24 +98,9 @@ include 'header.php';
     </a>
 
     <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php
+        draw_modal();
+    ?>
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
